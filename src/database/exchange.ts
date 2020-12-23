20f9967a7cb6c3c5fd4c613f5e2274c6b0ab1e67
심철환 <a@akaiv.com>
2020-12-23 23:31:33 +0900

    오늘의 환율을 항상 덮어씌우기

--- src/database/exchange.ts
+++ src/database/exchange.ts
@@ -1,8 +1,8 @@
 import { useEffect } from "react"
-import { selector, useRecoilValue } from "recoil"
+import { selector } from "recoil"
 import { latest, today } from "../utils/history"
 import { prevExchange } from "./polygon"
-import { addExchange, contentsState, databaseState } from "./database"
+import { addExchange, contentsState } from "./database"
 
 export const todayExchangeQuery = selector({
   key: "todayExchange",
@@ -12,18 +12,13 @@ export const todayExchangeQuery = selector({
   },
 })
 
-// If the database is loaded and today's exchange rate is empty,
-// a new one will be stored.
 export const useTodayExchange = () => {
-  const { state, contents } = useRecoilValue(databaseState)
-  const shouldFetch = state === "hasValue" && !contents.exchanges[today]
-
   useEffect(() => {
     const fetch = async () => {
       const { data } = await prevExchange()
       addExchange(today, data.results[0].c)
     }
 
-    shouldFetch && fetch()
-  }, [shouldFetch])
+    fetch()
+  }, [])
 }
